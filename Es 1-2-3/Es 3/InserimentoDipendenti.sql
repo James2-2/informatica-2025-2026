5c3df6c0d9693b4e3946c52ee6e494dcfcd5ae6d
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-10-16 23:16:05 +0200

    Create InserimentoDipendenti.sql

---
+++ Es 1-2-3/Es 3/InserimentoDipendenti.sql
@@ -0,0 +1,3 @@
+insert into dipendenti(cognome,nome,data_nascita,cap, citta, anzianita)
+values ('Rossi','Mario','2000-01-10','80013','Napoli', 25),
+('Verdi','Fabio','2001-09-15','50121','Firenze', 24);
