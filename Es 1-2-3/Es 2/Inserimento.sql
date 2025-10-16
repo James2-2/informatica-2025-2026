d2b447f49b3e4e762768cf40ba803f0be3e877fe
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-10-16 23:09:36 +0200

    Create Inserimento.sql

---
+++ Es 1-2-3/Es 2/Inserimento.sql
@@ -0,0 +1,3 @@
+insert into libri(isbn, titolo, autore, argomento, editore, anno, edizione)
+values ('AP1029384UDJC','Odi barbare','Giosuè Carducci','Risorgimento','Mondadori',1877,1920),
+('A23lWOSKMDJ23','Divina Commedia','Dante Alighieri','Religione','Einaudi',1321,2001);
