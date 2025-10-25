8d040272523dcc6580f129a3fa3a8a0cf1920218
Jack2250 <150335369+Jack2250@users.noreply.github.com>
2025-10-25 11:21:01 +0200

    Add files via upload

--- SQL/Lezioni/studenti/query.sql
+++ SQL/Lezioni/studenti/query.sql
@@ -1,22 +1,38 @@
 -- Funzioni di aggregazione
-select COUNT (*) as totaleClienti from mock_data -- Conta le persone e rinonima il campo in totale clienti
--- conta anche i NULL
+select COUNT (*) as totaleClienti -- Conta le persone e rinonima il campo in totale clienti e conta anche i NULL
+from mock_data 
 
-select count (nome) from studenti; -- così non conta i NULL
-select count (distinct nome) from studenti; -- così non conta i duplicati che hanno lo stesso nome
-select count (nome) from studenti WHERE nome = 'Andrea';
+select count (nome) -- così non conta i NULL
+from studenti; 
 
-select sum(voto) as sommaVoti from studenti;
-SELECT sum(voto) AS sommaVoti FROM studenti WHERE nome = 'A%'; -- somma i voti di chi ha il nome che inizia per A
+select count (distinct nome) -- così non conta i duplicati che hanno lo stesso nome
+from studenti;
 
-SELECT sum(voto) AS sommaVoti, count (nome) AS totale FROM studenti;
-SELECT sum(DISTINCT voto) AS sommaVoti, count (DISTINCT voto) AS totale FROM studenti;
+select count (nome) 
+from studenti 
+WHERE nome = 'Andrea';
+
+select sum(voto) as sommaVoti 
+from studenti;
+
+SELECT sum(voto) AS sommaVoti 
+FROM studenti 
+WHERE nome = 'A%'; -- somma i voti di chi ha il nome che inizia per A
+
+SELECT sum(voto) AS sommaVoti, 
+count (nome) AS totale 
+FROM studenti;
+
+SELECT sum(DISTINCT voto) AS sommaVoti, 
+count (DISTINCT voto) AS totale 
+FROM studenti;
 
 SELECT avg(voto) AS mediaClasse 
 from studenti; -- Average, fa la media
 
 SELECT avg(voto) as mediaVotiAndrea 
-FROM studenti WHERE nome LIKE 'Andrea';
+FROM studenti 
+WHERE nome LIKE 'Andrea';
 
 SELECT min(voto) AS votoMinimo 
 FROM studenti;
@@ -29,5 +45,34 @@ FROM voti
 GROUP BY cognome, nome; -- in base al cognome e al nome fa la media di tutti i voti
 
 SELECT cognome, nome, materia avg(voto) AS mediaVoti
-FROM voti
+FROM voti 
 GROUP BY cognome, nome, materia;
+
+SELECT cognome, nome, materia avg(voto) AS mediaVoti
+FROM voti 
+GROUP BY cognome, nome, materia
+having avg(voto) > 6;
+
+alter table voti add numInterrogazioni int;
+
+update voti
+set numInterrogazioni = 3;
+
+SELECT cognome, nome, materia avg(voto) AS mediaVoti, sum(numInterrogazioni) as numero_interrogazioni
+FROM voti 
+GROUP BY cognome, nome, materia
+having mediaVoti > 6 and numero_interrogazioni > 3; -- Si possono usare gli alias
+
+SELECT cognome, nome, materia, AVG(voto) AS mediaVoti
+FROM voti
+WHERE voto >= 6
+GROUP BY cognome, nome, materia
+HAVING mediaVoti > 7; -- having lavora sulle funzioni di aggregazione dopo group by, where lavora sulle tuple, sulle tabelle
+
+select voto
+from voti
+where materia = 'Informatica' and nome = 'Andrea';
+
+update voti
+set voto = 5
+where materia = 'Informatica' and nome = 'Andrea';
