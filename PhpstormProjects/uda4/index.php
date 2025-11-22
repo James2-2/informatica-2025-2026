dfc50977cda6790549299f9d3b735cc6cc3cf05e
James2-2 <150335369+James2-2@users.noreply.github.com>
2025-11-22 11:23:40 +0100

    Add files via upload

---
+++ PhpstormProjects/uda4/index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo 'ciao mondo'; //Output
+echo '<br>';
+echo 'Buongiorno a tutti';
+echo '<br>';
+
+$var = 10;
+echo 'Il valore della variabile è:  '.$var;
+echo '<br>';
+var_dump($var); //Stampa il valore della variabile e il tipo
+echo '<br>';
+
+$var2  = 2.50;
+echo 'Il valore della variabile è:  '.$var2;
+echo '<br>';
+var_dump($var2);
+
+echo '<br>';
+$var2 = 'ciao';
+var_dump($var2);
+
+//Costanti
+echo '<br>';
+echo M_PI; //
+echo '<br>';
+echo PHP_INT_MAX; //Massimo numero intero in php
+echo '<br>';
+echo PHP_INT_MIN;
