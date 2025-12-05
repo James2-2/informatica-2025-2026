bdc74b66f4cf04e6aea96609dbe9be60f0102789
James2-2 <150335369+James2-2@users.noreply.github.com>
2025-12-05 13:21:40 +0100

    Add files via upload

---
+++ PhpstormProjects/uda4/dicembre/stringheNumeri.php
@@ -0,0 +1,17 @@
+<?php
+echo "Manipolazione numeri e stringhe";
+$stringa = "ciao e buongiorno";
+echo "<br>";
+echo "<br>";
+
+echo "Strlen lunghezza stringa: ";
+echo strlen($stringa);
+echo "<br>";
+echo "Strrev Inverte la stringa: ";
+echo strrev($stringa);
+
+echo "<br>";
+echo ucfirst($stringa); //Mette maiuscola la prima lettera
+echo "<br>";
+echo ucwords($stringa); //Mette maiuscola la prima lettera di ogni parola
+
