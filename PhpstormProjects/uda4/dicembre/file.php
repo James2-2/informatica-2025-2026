9d40c72b96e6775c1c81e8ffed10142d366fbcc2
James2-2 <150335369+James2-2@users.noreply.github.com>
2025-12-19 11:53:05 +0100

    Add files via upload

--- PhpstormProjects/uda4/dicembre/file.php
+++ PhpstormProjects/uda4/dicembre/file.php
@@ -28,3 +28,46 @@ fclose($file);
 $file2 = fopen("prova.txt", "a"); //Aggiunge alla fine del file, non sovrascrive
 fwrite($file2, "Ciao a tutti 2");
 fclose($file2);
+
+$classe = [
+    "studente1" => ["nome1","cognome1", 8],
+    "studente2" => ["nome2","cognome2", 9],
+    "studente3" => ["nome3","cognome3", 5],
+    "studente4" => ["nome4","cognome4", 4],
+    "studente5" => ["nome5","cognome5", 1],
+    "studente6" => ["nome6","cognome6", 2],
+];
+
+$file = fopen("voti.txt","w");
+foreach ($classe as $stud){
+    $line = implode(", ",$stud).PHP_EOL; //Prende gli elementi dell'array e li converte in stringa perchè fwrite() scrive delle stringhe
+    fwrite($file, $line);
+}
+fclose($file);
+
+$file = fopen("voti.txt","w");
+foreach ($classe as $key => $stud){
+    $line = $key.", ".implode(", ",$stud).PHP_EOL;
+    fwrite($file, $line);
+}
+fclose($file);
+
+$datiFromFile = [];
+$file = fopen("voti.txt", "r");
+while($line = fgets($file) != false){
+    $datiFromFile[] = trim($line); //Toglie gli spazi
+}
+fclose($file);
+
+foreach($datiFromFile as $dati){
+    echo $dati."<br>";
+}
+
+echo $datiFromFile[1]."<br>";
+
+$frase = "Oggi è una bella giornata";
+$arrayFrase = explode(" ",$frase); //explode() converte da stringa ad array
+
+foreach($arrayFrase as $parola){
+    echo $parola."<br>";
+}
--- PhpstormProjects/uda4/dicembre/stringheNumeri.php
+++ PhpstormProjects/uda4/dicembre/stringheNumeri.php
@@ -1,17 +1,97 @@
 <?php
-echo "Manipolazione numeri e stringhe";
-$stringa = "ciao e buongiorno";
+echo "Manipolazione numeri e stringhe<br><br>";
+
+$stringa = "ciao e buongiorno 246452";
+
+echo "Stringa originale:". $stringa;
+echo "<br>";
+echo "strlen (lunghezza): " . strlen($stringa);
+echo "<br>";
+echo "strrev (invertita): " . strrev($stringa) . "<br>";
+echo "strtolower: " . strtolower($stringa) . "<br>";
+echo "strtoupper: " . strtoupper($stringa) . "<br>";
+echo "ucfirst: " . ucfirst($stringa) . "<br>";
+echo "ucwords: " . ucwords($stringa) . "<br><br>";
+
+echo "trim: " . trim($stringa) . "<br>";
+echo "ltrim: " . ltrim($stringa) . "<br>";
+echo "rtrim: " . rtrim($stringa) . "<br><br>";
+
+$array = explode(" ", $stringa);
+echo "explode: ";
+print_r($array);
+echo "<br>";
+
+echo "implode: " . implode("-", $array) . "<br><br>";
+
+/* sostituzione */
+echo "str_replace: " . str_replace("ciao", "Ciao", $stringa) . "<br><br>";
+
+/* substring */
+echo "substr (da posizione 5): " . substr($stringa, 5) . "<br>";
+echo "substr (5, 10): " . substr($stringa, 5, 10) . "<br><br>";
+
+/* ricerca */
+echo "strpos ('buongiorno'): " . strpos($stringa, "buongiorno") . "<br>";
+echo "strrpos ('o'): " . strrpos($stringa, "o") . "<br><br>";
+
+echo "strstr ('buongiorno'): " . strstr($stringa, "buongiorno") . "<br>";
+echo "stristr ('CIAO'): " . stristr($stringa, "CIAO") . "<br><br>";
+
+/* formattazione */
+echo sprintf("Testo formattato: %s", $stringa) . "<br>";
+
+printf("printf: %s<br><br>", $stringa);
+
+/* numeri */
+$numero = 246452.5678;
+echo "number_format: " . number_format($numero, 2, ",", ".") . "<br><br>";
+
+echo "addslashes: " . addslashes($stringa) . "<br>";
+echo "stripslashes: " . stripslashes(addslashes($stringa)) . "<br>";
 echo "<br>";
 echo "<br>";
 
-echo "Strlen lunghezza stringa: ";
-echo strlen($stringa);
-echo "<br>";
-echo "Strrev Inverte la stringa: ";
-echo strrev($stringa);
 
-echo "<br>";
-echo ucfirst($stringa); //Mette maiuscola solo la prima lettera
-echo "<br>";
-echo ucwords($stringa); //Mette maiuscola la prima lettera di ogni parola
 
+//NUMERI
+$num = -345.234;
+echo abs($num);
+echo "<br>";
+echo ceil($num);
+echo "<br>";
+echo floor($num);
+echo "<br>";
+echo round($num);
+echo "<br>";
+echo "Valore random".mt_rand();
+echo "<br>";
+echo rand();
+echo "<br>";
+echo min($num);
+echo "<br>";
+echo max($num);
+echo "<br>";
+echo sqrt($num);
+echo "<br>";
+echo pow($num, 2);
+echo "<br>";
+echo intdiv($num, 2);
+echo "<br>";
+echo number_format($num);
+echo "<br>";
+echo is_numeric($num);
+echo "<br>";
+echo is_int($num);
+echo "<br>";
+echo is_float($num);
+echo "<br>";
+echo intval($num);
+echo "<br>";
+echo floatval($num);
+echo "<br>";
+echo pi();
+echo "<br>";
+echo log($num);
+echo "<br>";
+echo exp($num);
