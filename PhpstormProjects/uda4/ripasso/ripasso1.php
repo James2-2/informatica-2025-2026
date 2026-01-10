20dfd0158b967bf548317e44e3ee3846e11e0220
James2-2 <150335369+James2-2@users.noreply.github.com>
2026-01-10 11:18:28 +0100

    Add files via upload

---
+++ PhpstormProjects/uda4/ripasso/ripasso1.php
@@ -0,0 +1,56 @@
+<?php
+
+//Operatori
+
+$num = 5;
+if($num == "5"){
+    echo "Uguali";
+}else{
+    echo "Diversi";
+}
+
+echo "<br>";
+
+if($num === "5"){
+    echo "Uguali";
+}else{
+    echo "Diversi";
+}
+
+echo "<br>";
+
+$risultato = $num > 5 ? "Maggiore di 5" : "Minore o uguale a 5"; //Operatore ternario condizionale
+echo $risultato;
+
+echo "<br>";
+
+$risultato2 = $nome ?? "Non esiste o è NULL"; //Operatore Coalescing
+echo $risultato2;
+
+echo "<br>";
+$x = 5;
+$y = 6;
+echo $x <=> $y; //Operatore Spaceship: ritorna -1 se il valore a sinistra è più piccolo, 1 se il contrario, 0 se uguali
+
+echo "<br>";
+
+
+//Strutture dati
+
+$array = [10,30,100];
+echo $array[1];
+
+echo "<br>";
+
+$arrayAssociativo = [
+    "nome" => "Giovanni",
+    "cognome" => "Rossi",
+    "eta" => 10
+];
+echo $arrayAssociativo["cognome"];
+
+echo "<br>";
+
+
+
+//Funzioni
