08f63e82cd05ed929048ffc197530fdc83ec4a06
James2-2 <150335369+James2-2@users.noreply.github.com>
2026-03-21 10:25:51 +0100

    Add files via upload

--- PhpstormProjects/upload/upload.php
+++ PhpstormProjects/upload/upload.php
@@ -1,5 +1,47 @@
 <?php
-$tmpPath = $_FILES["file"]["tmp_name"]; //Salva temporaneamente il file nel percorso tmp_name
-$destination = "uploads/";
-move_uploaded_file($tmpPath, $destination);
+$allowed = ["jpg","png","pdf"]; //Estensione dei file che possiamo caricare
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    if($_FILES["file"]["error"] === UPLOAD_ERR_OK){ //C'è stato un errore o no?
+
+        $tmpPath = $_FILES["file"]["tmp_name"]; //Salva temporaneamente il file nel percorso tmp_name
+        //echo $tmpPath."<br>";
+        $originalName = basename($_FILES["file"]["name"]); //basename ritorna il nome del file senza spazi
+        //echo $originalName."<br>";
+        $username = $_POST["nome"];
+        //echo $username."<br";
+
+        //Extension check
+        $ext = strtolower(pathinfo($originalName, PATHINFO_EXTENSION));
+        if(!in_array($ext, $allowed)){
+            http_response_code(403);
+            $msg = "Estensione non autorizzata";
+            include "message.php";
+            exit();
+        }
+
+        $maxSize = 2 * 1024 * 1024; //1 MegaByte
+        $size = $_FILES["file"]["size"];
+        if($size > $maxSize){
+            http_response_code(413);
+            $msg = "File troppo grande";
+            include "message.php";
+            exit();
+        }
+
+        $userDir = "uploads/".$username;
+        if(!is_dir($userDir)){
+            mkdir($userDir, 0755);
+        }
+        $destination = $userDir."/".$originalName;
+        move_uploaded_file($tmpPath, $destination);
+        $msg = "File caricato corretamente";
+        include "message.php";
+    }else{
+        $msg = "Errore nel caricamento";
+        http_response_code(500);
+        include "message.php";
+    }
+}
+
 ?>
